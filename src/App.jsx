7184e50d0ad497da685c7415530b0607f2e21efd
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-01-31 22:53:41 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import Explanation from './Liff/Explanation'
 import LoadingError from './Liff/LoadingError'
 import Compress from './Liff/Compress'
 import ConvertDownload from './Liff/ConvertDownload'
-import Carousel from './Liff/how-to-use/Carousel'
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/explanation' element={<Carousel />} />
+        <Route path='/explanation' element={<Explanation />} />
         <Route path='/convert' element={<ConvertDownload />}/>
       </Routes>
     </>
