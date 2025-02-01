3b69eecff8e9cc82ec456deba701d571feafdf90
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-01 17:26:47 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './Liff/Home'
-import Generate from './Liff/Generate'
-import Explanation from './Liff/Explanation'
-import LoadingError from './Liff/LoadingError'
-import Compress from './Liff/Compress'
-import ConvertDownload from './Liff/ConvertDownload'
 import Carousel from './Liff/how-to-use/Carousel'
-import TextDownload from './Liff/TextDownload'
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Carousel />} />
         <Route path='/explanation' element={<Carousel />} />
-        <Route path='/convert' element={<TextDownload />}/>
       </Routes>
     </>
   )
