a79513ef8d8a2a354bed647cf18b51ea52083214
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-05 15:41:13 +0800

    update

--- src/Liff/Loading.jsx
+++ src/Liff/Loading.jsx
@@ -43,7 +43,7 @@ const Loading = ({ isDone }) => {
                                 className="w-full min-h-72 max-h-72"
                             />
                         ) : (
-                          <iframe src={ads.url} className="w-full min-h-72 max-h-72" allow="autoplay" style={{ pointerEvents: "none" }}></iframe>
+                          <iframe src={`${ads.url}?autoplay=1&controls=0&disablekb=1`} className="w-full min-h-72 max-h-72" allow="autoplay" style={{ pointerEvents: "none" }}></iframe>
                         )
                     ) : (
                         <p>Loading media...</p>
