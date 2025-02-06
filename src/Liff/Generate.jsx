bf6d695d86c34c750a935f36de77cff83b3534ed
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-06 10:21:31 +0800

    Update

--- src/Liff/Generate.jsx
+++ src/Liff/Generate.jsx
@@ -14,6 +14,7 @@ function Generate({prompt, userId}) {
     const [isLoading, setIsLoading] = useState(false)
     const [home, setHome] = useState(false)
     const [copy, setCopy] = useState(false)
+    const [isDone, setIsDone] = useState(false)
     const handleCompress = () => {
         setIsLoading(true)
         axios.get(
@@ -54,7 +55,7 @@ function Generate({prompt, userId}) {
         />
     }
     if(isLoading){
-        return <Loading />
+        return <Loading onLoadingComplete={() => setIsLoading(false)} />;
     }
 
   const handleCopy = () => {
