7e6c8a625557fad0762ca6dbc60c68d302d3b98b
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-06 09:29:51 +0800

    Update

--- src/Liff/Loading.jsx
+++ src/Liff/Loading.jsx
@@ -2,28 +2,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Loading = ({ isDone }) => {
-    const [ads, setAds] = useState({});
-    const [newAd, setNewAd] = useState(null);
-    const [isWaiting, setIsWaiting] = useState(false);
+    const [ads, setAds] = useState(null);
+    const [isWaiting, setIsWaiting] = useState(true);
 
     useEffect(() => {
-        let interval;
-
         const fetchAds = async () => {
             try {
-                const response = await axios.get("https://reuvindevs.com/liff/public/api/firebase-files")
+                const response = await axios.get("https://reuvindevs.com/liff/public/api/firebase-files");
                 console.log("Fetched Ads Data:", response.data);
 
-                if (!isWaiting) {
-                    setAds(response.data);
-                    setIsWaiting(true);
+                setAds(response.data);
 
-                    setTimeout(() => {
-                        setIsWaiting(false);
-                    }, 15000);
-                } else {
-                    setNewAd(response.data);
-                }
+                setTimeout(() => {
+                    setIsWaiting(false);
+                }, 15000);
             } catch (error) {
                 console.error("Error fetching ads:", error);
             }
@@ -31,28 +23,8 @@ const Loading = ({ isDone }) => {
 
         if (!isDone) {
             fetchAds();
-
-            interval = setInterval(() => {
-                fetchAds();
-            }, 15000);
         }
-
-        return () => {
-            clearInterval(interval);
-        };
     }, [isDone]);
-
-    useEffect(() => {
-        if (!isWaiting && newAd) {
-            setAds(newAd);
-            setNewAd(null);
-            setIsWaiting(true);
-
-            setTimeout(() => {
-                setIsWaiting(false);
-            }, 15000);
-        }
-    }, [isWaiting, newAd]);
 
     return (
         <div className="min-h-screen bg-blue-100 flex justify-center items-center">
@@ -61,7 +33,9 @@ const Loading = ({ isDone }) => {
                     文章の作成が完了しました
                 </div>
                 <div className="min-h-72 border-2 border-black bg-white mb-2 overflow-auto overflow-x-hidden">
-                    {ads.url ? (
+                    {isWaiting || !ads ? (
+                        <p>Please wait....</p>
+                    ) : (
                         ads.mime_type && ads.mime_type.includes("image") ? (
                             <img
                                 src={ads.url}
@@ -76,8 +50,6 @@ const Loading = ({ isDone }) => {
                                 style={{ pointerEvents: "none" }}
                             ></iframe>
                         )
-                    ) : (
-                        <p>Please wait....</p>
                     )}
                 </div>
             </div>
